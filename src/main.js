73b98e635d39f80946784941649ec7ab30ddfd9b
Hayden <haosong0323@gmail.com>
2020-09-04 15:35:04 +0800

    添加eslint忽略文件

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-import './assets/styles/tailwind.css'
+import './assets/styles/tailwind.less'
 
 Vue.config.productionTip = false
 
