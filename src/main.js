9efc6385ab7ebd7609d8f5c2458d53452498f804
Hayden <haosong0323@gmail.com>
2020-09-17 10:32:07 +0800

    引入font-awesome

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import router from './router'
 import store from './store'
 
 import './assets/styles/tailwind.less'
+import 'font-awesome/css/font-awesome.min.css'
 
 Vue.config.productionTip = false
 
